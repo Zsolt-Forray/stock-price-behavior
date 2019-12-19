bd8815b550d093d8d2a8eebdcf1727e0a1ab6d4a
Zsolt Forray <forray.zsolt@gmail.com>
2019-12-19 13:27:14 +0100

    Add install_requires

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from setuptools import setup
 
-setup(
-    version="0.0.1",
-    author="Zsolt Forray",
-    py_modules=["tests.context.py"]
-)
+
+setup(  name="stock_price_behavior",
+        version="0.0.1",
+        author="Zsolt Forray",
+        license="MIT",
+        packages="stock_price_behavior",
+        install_requires=[
+            "matplotlib == 2.2.4",
+            "numpy == 1.16.3",
+            "pandas == 0.24.2",
+            "setuptools == 39.0.1",
+            ],
+    )
